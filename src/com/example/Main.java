7e25b18254831af14a10d0d24d335b0596791b2b
jason.chan <jason.chan@team-c.com>
2019-07-19 10:20:13 +0800

    upload starter

---
+++ src/com/example/Main.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+
+@EnableJms
+@SpringBootApplication
+public class Main {
+
+	 @Autowired
+	    private JmsTemplate jmsTemplate;
+
+	    public static void main(String[] args) {
+	        SpringApplication.run(Main.class, args);
+	    }
+
+	    public void run(String... strings) throws Exception {
+	    	//jmsTemplate
+	        sendMessage("Hello World!");
+	    }
+
+	    public void sendMessage(String text) {
+	        System.out.println(String.format("Sending '%s'", text));
+	        this.jmsTemplate.convertAndSend("example", text);
+	    }
+
+	    @JmsListener(destination = "example")
+	    public void receiveMessage(String text) {
+	        System.out.println(String.format("Received '%s'", text));
+	    }
+
+}
